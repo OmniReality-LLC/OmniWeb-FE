e5f5f2980b293630a5c4a2a21a69d9cbb4465147
Anthony Lee <anthonykilde@outlook.com>
2023-06-28 10:48:31 -0500

    Fixed tab ttitle

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Nicolaas's Portfolio",
-  description: 'Create. Inspire. Design.',
+  title: "OmniReality LLC",
+  description: 'Web | DaaS | XR',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
