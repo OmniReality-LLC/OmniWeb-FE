48ce98abd47433c5bc7dd33cbfd535011e91d1f3
Anthony Lee <anthonykilde@outlook.com>
2023-06-28 10:28:10 -0500

    Tracked all files

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Lottie from 'lottie-react'
+import styles from './page.module.css'
+import construction from '../public/lottie/construction.json';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className = {styles.mainContainer}>
+      <Lottie animationData={construction} autoplay loop />
+      </div>
+    </main>
+  )
+}
