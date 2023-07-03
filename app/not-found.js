60d2db6f68bd0489a92d2834a75325a023ab6dba
Anthony Lee <anthonykilde@outlook.com>
2023-07-02 23:14:29 -0500

    Fixed 404

--- app/not-found.js
+++ app/not-found.js
@@ -5,7 +5,7 @@ import animationData from '../public/lottie/OmniReality404Anim.json';
 export default function Custom404() {
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', background: "black"}}>
-            <Lottie animationData={animationData} loop={false} style={{marginTop:'-50px', height: 'calc(100vh - 6rem)'}}/>
+            <Lottie animationData={animationData} loop={false} style={{marginTop:'-50px', height: 'calc(100vh - 6rem)', minWidth: '950px', minHeight: '420px'}}/>
         </div>
     );
 }
