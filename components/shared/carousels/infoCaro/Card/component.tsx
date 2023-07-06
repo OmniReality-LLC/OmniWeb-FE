3fc4901cd5194238df0393562354d937975bb36b
Anthony Lee <anthonykilde@outlook.com>
2023-07-05 23:03:33 -0500

    Fixed landscape mode on 375px and up

--- components/shared/carousels/infoCaro/Card/component.tsx
+++ components/shared/carousels/infoCaro/Card/component.tsx
@@ -23,7 +23,7 @@ export default function CaroCard() {
                     </div>
 
                     <div className={styles.btnContainer}>
-                        <SlideGlowLinkBtn text={'View'} linkTo={''} marginL='0' defaultStyle={false} className={styles.myButton} marginB='2.5%' marginT='10%' marginR='0' />
+                        <SlideGlowLinkBtn text={'View'} linkTo={''} marginL='0' defaultStyle={false} className={styles.myButton} marginB='2.5%' marginT='10%' marginR='0' textSize='16px' />
                     </div>
                 </div>
             </div>
