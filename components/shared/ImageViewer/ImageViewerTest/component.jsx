9d8dc31bf9e82036ffcce73790fa65ef10c86283
Anthony Lee <anthonykilde@outlook.com>
2023-10-18 21:03:20 -0700

    small update

--- components/shared/ImageViewer/ImageViewerTest/component.jsx
+++ components/shared/ImageViewer/ImageViewerTest/component.jsx
@@ -9,46 +9,44 @@ const leftArrowSVGString = '<svg aria-hidden="true" class="pswp__icn" viewBox="0
 
 export default function SimpleGallery(props) {
 
-  const [activeIndex, setActiveIndex] = useState(0); // New state added as requested
-
-
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [initialPosition, setInitialPosition] = useState(0);
+  const [distanceDragged, setDistanceDragged] = useState(0); // Declare here
   const scrollerRef = useRef(null);
+  const [handleScroll, setHandleScroll] = useState(null);
 
+  useEffect(() => {
+    // Initialize lightbox
+    let lightbox = new PhotoSwipeLightbox({
+      gallery: '#' + props.galleryID,
+      children: 'a',
+      arrowPrevSVG: leftArrowSVGString,
+      arrowNextSVG: leftArrowSVGString,
+      mainClass: 'MYSETCLASS',
+      pswpModule: () => import('photoswipe'),
+    });
+    lightbox.init();
 
-  // First useEffect to define handleScroll
-const [handleScroll, setHandleScroll] = useState(null);
-
-useEffect(() => {
-  // Initialize lightbox
-  let lightbox = new PhotoSwipeLightbox({
-    gallery: '#' + props.galleryID,
-    children: 'a',
-    arrowPrevSVG: leftArrowSVGString,
-    arrowNextSVG: leftArrowSVGString,
-    mainClass: 'MYSETCLASS',
-    pswpModule: () => import('photoswipe'),
-  });
-  lightbox.init();
-
-  // Attach scroll event listener
-  if (scrollerRef.current) {
-    scrollerRef.current.addEventListener('scroll', handleScroll);
-  }
-
-  // Cleanup
-  return () => {
-    // Destroy lightbox
-    lightbox.destroy();
-    lightbox = null;
-
-    // Remove scroll event listener
+    // Attach scroll event listener
     if (scrollerRef.current) {
-      scrollerRef.current.removeEventListener('scroll', handleScroll);
+      scrollerRef.current.addEventListener('scroll', handleScroll);
     }
-  };
-}, []);
 
-useEffect(() => {
+    // Cleanup
+    return () => {
+      // Destroy lightbox
+      lightbox.destroy();
+      lightbox = null;
+
+      // Remove scroll event listener
+      if (scrollerRef.current) {
+        scrollerRef.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
     const scrollHandler = () => {
       if (!scrollerRef.current) return;
       const elementWidth = scrollerRef.current.offsetWidth;
@@ -62,10 +60,10 @@ useEffect(() => {
 
     setHandleScroll(() => scrollHandler);
 
-}, [activeIndex]);
+  }, [activeIndex]);
 
-// Second useEffect to attach the listener
-useEffect(() => {
+  // Second useEffect to attach the listener
+  useEffect(() => {
     if (scrollerRef.current && handleScroll) {
       scrollerRef.current.addEventListener('scroll', handleScroll);
     }
@@ -75,74 +73,112 @@ useEffect(() => {
         scrollerRef.current.removeEventListener('scroll', handleScroll);
       }
     };
-}, [handleScroll]);
+  }, [handleScroll]);
 
 
-function switchActiveIndex(index) {
+  function switchActiveIndex(index) {
+    movePage(index);
+  }
 
-
-  setActiveIndex(index);
-  movePage(index);
-
-}
-
-function movePage(index){
-  const scroller = scrollerRef.current;
-  if (scroller) {
+  function movePage(index) {
+    const scroller = scrollerRef.current;
+    if (scroller) {
       const spotsToMove = index - activeIndex;
       let pixelsToMove = scroller.offsetWidth * spotsToMove;
       scroller.scrollBy({ left: pixelsToMove, behavior: 'smooth' });
 
+    }
+
   }
 
 
-}
+  function handleMouseDown(e) {
+    setIsDragging(true);
+    setInitialPosition(e.clientX); // Store the initial x position when starting to drag.
+  }
 
-return (
 
-  <div className={styles.gallery}>
-    <div className={styles.gallery_scroller} ref={scrollerRef} id={props.galleryID}>
-      {props.images.map((myObject, index) =>
-        <div className={styles.carTestItem} key={'webDesignCarousel' + 'ID:' + index}>
-          <div className={`${styles.imageViewerMain} pswp-gallery`} style={props.padding ? { paddingLeft: props.padding, paddingRight: props.padding } : {}}>
-            {myObject.map((image, index) => (
-              <a
-                href={image.largeURL}
-                data-pswp-width={image.width}
-                data-pswp-height={image.height}
-                key={props.galleryID + '-' + index}
-                target="_blank"
-                rel="noreferrer"
-                className={styles.imageLinkContainer}
-              >
-                <CldImage width={image.width} height={image.height} src={image.thumbnailURL} alt="Image of a featured item" className={styles.myImg} />
+  function handleMouseUp() {
+    setIsDragging(false);
 
-              </a>
-            ))}
+    const scroller = scrollerRef.current;
+    if (scroller) {
+      const elementWidth = scroller.offsetWidth;
+      const currentPage = Math.round((scroller.scrollLeft + distanceDragged) / elementWidth);
+
+      const pixelsToMove = currentPage * elementWidth - scroller.scrollLeft;
+      scroller.scrollBy({ left: pixelsToMove, behavior: 'smooth' });
+    }
+
+    setDistanceDragged(0); // Reset the distance dragged for the next drag.
+  }
+
+  function handleMouseMove(e) {
+    if (!isDragging) return;
+
+    const currentPosition = e.clientX;
+    const dx = initialPosition - currentPosition; // Difference between initial and current x position.
+
+    const scroller = scrollerRef.current;
+    if (scroller) {
+      scroller.scrollLeft += dx; // Scroll by the difference.
+    }
+    setDistanceDragged(prevDistance => prevDistance + dx);
+    setInitialPosition(currentPosition); // Update the initial position for the next movement.
+  }
+
+  return (
+
+    <div className={styles.gallery}>
+      <div
+        className={styles.gallery_scroller}
+        ref={scrollerRef}
+        id={props.galleryID}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      >
+        {props.images.map((myObject, index) =>
+          <div className={styles.carTestItem} key={'webDesignCarousel' + 'ID:' + index}>
+            <div className={`${styles.imageViewerMain} pswp-gallery`} style={props.padding ? { paddingLeft: props.padding, paddingRight: props.padding } : {}}>
+              {myObject.map((image, index) => (
+                <a
+                  href={image.largeURL}
+                  data-pswp-width={image.width}
+                  data-pswp-height={image.height}
+                  key={props.galleryID + '-' + index}
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles.imageLinkContainer}
+                >
+                  <CldImage width={image.width} height={image.height} src={image.thumbnailURL} alt="Image of a featured item" className={styles.myImg} />
+
+                </a>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+
+      {/* Dots */}
+
+      <div className={styles.dotsContainer}>
+        {props.images.map((_, index) => (
+          <Dot
+            key={index}
+            isActive={index === activeIndex}
+            onClick={switchActiveIndex}
+            index={index}
+
+          />
+        ))}
+      </div>
+
+      {/* Controls */}
+
     </div>
 
-    {/* Dots */}
-
-    <div className={styles.dotsContainer}>
-      {props.images.map((_, index) => (
-        <Dot
-          key={index}
-          isActive={index === activeIndex}
-          onClick={switchActiveIndex}
-          index={index}
-
-        />
-      ))}
-    </div>
-
-    {/* Controls */}
-
-  </div>
-
-);
+  );
 }
 
 
